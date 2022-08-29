819ee1cd0e5e24282704f63cb6aab344e9fb25cc
Mateus Santos <a.mateussm1.5@gmail.com>
2022-08-29 16:19:51 -0300

    :wrench: | Criou construtor e valorFinal

--- src/model/entidades/Pagamento.java
+++ src/model/entidades/Pagamento.java
@@ -7,6 +7,11 @@ public class Pagamento {
 	private EstadoPagamento estado;
 	private Double valor;
 	
+	public Pagamento(Integer id, EstadoPagamento estado, Double valor) {
+		this.id = id;
+		this.estado = estado;
+		this.valor = valor;
+	}
 	public Integer getId() {
 		return id;
 	}
@@ -26,5 +31,18 @@ public class Pagamento {
 		this.valor = valor;
 	}
 	
+	public Double valorFinal() {
+		return this.getValor();
+	}
+	
+	public String relatorio() {
+		StringBuilder stringBuilder = new StringBuilder();
+		
+		stringBuilder.append("Id: " + this.getId());
+		stringBuilder.append(", Estado: " + this.getEstado());
+		stringBuilder.append(", Valor: " + this.getValor());
+		
+		return stringBuilder.toString(); 
+	}
 	
 }
