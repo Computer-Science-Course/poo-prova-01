93568261ac096f5d1b32a73e7beee4f8debbbdc6
Mateus Santos <a.mateussm1.5@gmail.com>
2022-08-29 15:11:48 -0300

    :tada: | Menu de opcoes

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -14,10 +14,21 @@ public class Programa {
 		
 		System.out.print("Entre com o número de pagamentos: ");
 		Integer numero_pagamentos = scanner.nextInt();
+		scanner.nextLine();
 		
 		if(numero_pagamentos != null) {
 			for(Integer i = 1; i <= numero_pagamentos; i++) {
 				System.out.printf("Pagamento #%d.\n", i);
+				System.out.print("É [C]artão ou [B]oleto (C/B)? ");
+				String resposta = scanner.nextLine();
+				if(resposta.toUpperCase().equals("C")) {
+					System.out.print("Escolheu cartão.");					
+				} else if(resposta.toUpperCase().equals("B")) {
+					System.out.print("Escolheu boleto.");										
+				} else {
+					System.out.print("Escolheu opção inválida.");															
+				}
+				
 			}
 		}
 		
